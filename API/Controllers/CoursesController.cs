310578158c073aea65f593938e858dc94e05dd7c
alfanaisy <alfanaisy7@gmail.com>
2024-09-18 10:10:51 +0700

    Initialize Project

---
+++ API/Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+using Entity;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class CoursesController: BaseController
+{
+    private readonly StoreContext _context;
+
+    public CoursesController(StoreContext context)
+    {
+      _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Course>>> GetCourses()
+    {
+      return await _context.Courses.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Course>> GetCourse(Guid id)
+    {
+      return await _context.Courses.FindAsync(id);
+    }
+
+}
