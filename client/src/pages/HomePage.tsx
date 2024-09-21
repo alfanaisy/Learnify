e1d202f62e1f370f0c7c63625ab6879c84a8d182
alfanaisy <alfanaisy7@gmail.com>
2024-09-21 20:57:37 +0700

    Make Course card reusable

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,9 +1,32 @@
-import Courses from '../components/Courses';
+import { useEffect, useState } from 'react';
+import { Course } from '../models/course';
+import agent from '../actions/agent';
+
+import { Row } from 'antd';
+import { PaginatedData } from '../models/paginatedData';
+import ShowCourses from '../components/ShowCourses';
 
 const HomePage = () => {
+  const [data, setData] = useState<PaginatedData<Course>>();
+
+  useEffect(() => {
+    agent.Courses.list().then((response) => {
+      setData(response);
+    });
+  }, []);
+
   return (
-    <div>
-      <Courses />
+    <div className="course">
+      <div className="course__header">
+        <h1>What to learn next?</h1>
+        <h2>New Courses picked just for you...</h2>
+      </div>
+      <Row gutter={[24, 32]}>
+        {data &&
+          data.data.map((course) => {
+            return <ShowCourses key={course.id} course={course} />;
+          })}
+      </Row>
     </div>
   );
 };
